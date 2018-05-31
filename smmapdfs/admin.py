4b5903914e4d1701cb4e8506357082aa73418143
Timothy Hobbs <timothy@hobbs.cz>
2018-05-31 16:21:32 +0200

    Add ability to configure help text for email context

--- smmapdfs/admin.py
+++ smmapdfs/admin.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2016 o.s. Auto*Mat
 from django.contrib import admin
+from django.conf import settings
+from django import forms
 
 from import_export.admin import ImportExportMixin
 
 from related_admin import RelatedFieldAdmin
-
 
 from . import actions
 from . import models
@@ -30,8 +31,23 @@ class PdfSandwichFieldAdminMixin(ImportExportMixin, RelatedFieldAdmin):
     list_editable = ('x', 'y', 'pdfsandwich_type')
 
 
+class EmailForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            more_attrs = settings.SMMAPDFS_EMAIL_CONTEXT_HELP
+        except AttributeError:
+            more_attrs = ""
+        self.fields['template'].help_text = "You can use the following variables in the email context:<br/>{{download_link|safe}} - Url for downlaoding the pdf sandwich\n"
+
+    class Meta:
+        model = models.PdfSandwichEmail
+        exclude = ()
+
+
 @admin.register(models.PdfSandwichEmail)
 class PdfSandwichEmailAdmin(ImportExportMixin, RelatedFieldAdmin):
+    form = EmailForm
     list_display = (
         'pdfsandwich_type',
         'language',
